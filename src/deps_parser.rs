91099d72b011760b017e4727b76033f6c640053f
Lauren N. Liberda <lauren@selfisekai.rocks>
2022-06-26 01:31:02 +0200

    support the other way of using vars in urls

--- src/deps_parser.rs
+++ src/deps_parser.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 use anyhow::Result;
 use pyo3::prelude::*;
+use pyo3::type_object::PyTypeObject;
 use pyo3::types::{PyDict, PyString};
 
 use crate::types::deps::DepsSpec;
@@ -33,6 +34,43 @@ pub fn parse_deps(path: &Path) -> Result<DepsSpec> {
             Some(globals),
         )
         .unwrap();
+
+        // apparently sometimes they use "{var_name}" and not Var('var_name')
+        for (dep_key, dep_val) in globals
+            .get_item("deps")
+            .unwrap()
+            .downcast::<PyDict>()
+            .unwrap()
+        {
+            let key = dep_key.downcast::<PyString>().unwrap();
+            if dep_val.is_instance(PyString::type_object(py)).unwrap() {
+                py.run(
+                    &format!(
+                        "deps[{0}] = deps[{0}].format(**vars)",
+                        serde_json::to_string(&key.to_string()).unwrap()
+                    ),
+                    Some(globals),
+                    Some(globals),
+                )
+                .unwrap();
+            } else if dep_val.is_instance(PyDict::type_object(py)).unwrap()
+                && dep_val
+                    .downcast::<PyDict>()
+                    .unwrap()
+                    .get_item("url")
+                    .is_some()
+            {
+                py.run(
+                    &format!(
+                        "deps[{0}]['url'] = deps[{0}]['url'].format(**vars)",
+                        serde_json::to_string(&key.to_string()).unwrap()
+                    ),
+                    Some(globals),
+                    Some(globals),
+                )
+                .unwrap();
+            }
+        }
 
         // something something "you should convert the Py* types instead of using JSON as intermediate" what about no :chad:
         let result = py
